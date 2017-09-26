c909fd34e87b396589853c33eb2fa3f8be84e951
srujanbelde <srujanbelde18@gmail.com>
2017-09-26 08:27:47 +0530

    Update LightScript2.cs

--- Assets/Scripts/LightScript2.cs
+++ Assets/Scripts/LightScript2.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightScript2 : MonoBehaviour {
+public class LightScript2 : MonoBehaviour
+{
 
     public Light light;
     public GameObject goLightSwitch;
     private bool _switchon;
     private GameObject _upposition;
     private GameObject _downposition;
-    public AudioClip switchOn;
+    public AudioClip _switchOn;
     public AudioSource _audioSource;
 
     void Awake()
@@ -17,6 +18,8 @@ public class LightScript2 : MonoBehaviour {
         _upposition = GameObject.Find("upPosition1");
         _downposition = GameObject.Find("downPosition1");
     }
+
+    /*
     void OnMouseDown()
     {
         if (_switchon)
@@ -24,7 +27,7 @@ public class LightScript2 : MonoBehaviour {
             _switchon = false;
             goLightSwitch.transform.position = new Vector3(_downposition.transform.position.x, _downposition.transform.position.y, _downposition.transform.position.z);
             light.enabled = false;
-            _audioSource.clip = switchOn;
+            _audioSource.clip = _switchOn;
             _audioSource.Play();
         }
         else if (!_switchon)
@@ -32,8 +35,52 @@ public class LightScript2 : MonoBehaviour {
             _switchon = true;
             goLightSwitch.transform.position = new Vector3(_upposition.transform.position.x, _upposition.transform.position.y, _upposition.transform.position.z);
             light.enabled = true;
-            _audioSource.clip = switchOn;
+            _audioSource.clip = _switchOn;
             _audioSource.Play();
         }
     }
+    */
+
+//    /*
+    bool triggered=true;
+    bool collision;
+    private void OnTriggerEnter(Collider other)
+    {
+        collision=true;   
+    }
+    private void OntriggerStay(Collider other)
+    {
+        collision=true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        collision=false;
+    }
+
+    void Update()
+    {
+        if(collision==true)    
+        {
+            if (SteamVR_Controller.Input(SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost)).GetHairTriggerDown())
+            {
+                if (triggered)
+                {
+                    triggered = false;
+                    goLightSwitch.transform.position = new Vector3(_downposition.transform.position.x, _downposition.transform.position.y, _downposition.transform.position.z);
+                    light.enabled = false;
+                    _audioSource.clip = _switchOn;
+                    _audioSource.Play();
+                }
+                else if (!triggered)
+                {
+                    triggered = true;
+                    goLightSwitch.transform.position = new Vector3(_upposition.transform.position.x, _upposition.transform.position.y, _upposition.transform.position.z);
+                    light.enabled = true;
+                    _audioSource.clip = _switchOn;
+                    _audioSource.Play();
+                }
+            }
+        }
+    }
+//*/
 }
